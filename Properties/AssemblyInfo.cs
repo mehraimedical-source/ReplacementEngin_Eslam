270337cb8b00bb55c52016bbd6c52f9c829d9471
mehraimedical-source <mehr.ai.medical@gmail.com>
2026-09-19 16:20:24 +0330

    Add real-time replacement engine core

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReplacementEngin_Eslam")]
+[assembly: AssemblyDescription("Real-time evidence-driven medical report replacement engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReplacementEngin_Eslam")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7e15d05b-2e64-4d2d-a7d2-30f3a5dfd6f4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
