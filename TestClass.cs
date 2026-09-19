fa2bd637df1bd7b334f723791755b11c24bf7e9c
mehraimedical-source <mehr.ai.medical@gmail.com>
2026-09-19 14:47:45 +0330

    Add TestClass with double addition method

---
+++ TestClass.cs
@@ -0,0 +1,10 @@
+namespace ReplacementEngin_Eslam
+{
+    public class TestClass
+    {
+        public double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
