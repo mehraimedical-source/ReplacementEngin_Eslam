8ed11a42e2cca73b89b4cc9340eea3d0b5cbe717
mehraimedical-source <mehr.ai.medical@gmail.com>
2026-09-19 16:54:55 +0330

    Add Windows Forms client for ReplacementEngine

---
+++ ReplacementEngin_Eslam.WinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ReplacementEngin_Eslam.WinForms")]
+[assembly: AssemblyDescription("Windows Forms test client for ReplacementEngine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReplacementEngin_Eslam.WinForms")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7c2a675a-59d9-4a73-82a8-44b9af5a2711")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
