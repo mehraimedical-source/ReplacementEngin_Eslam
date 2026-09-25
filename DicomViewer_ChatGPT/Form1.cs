347417c578da446e0511140186e676010f4e0869
mehraimedical-source <mehr.ai.medical@gmail.com>
2026-09-25 11:57:31 +0330

    Add folder loading test host

--- DicomViewer_ChatGPT/Form1.cs
+++ DicomViewer_ChatGPT/Form1.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace DicomViewer_ChatGPT
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() { InitializeComponent(); }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select a folder containing a DICOM study/series";
+                if (Directory.Exists(txtFolder.Text)) dialog.SelectedPath = txtFolder.Text;
+                if (dialog.ShowDialog(this) == DialogResult.OK) txtFolder.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UseWaitCursor = true;
+                lblState.Text = "Scanning and loading DICOM...";
+                Application.DoEvents();
+                dicomViewer.ActiveFolder(txtFolder.Text);
+                lblState.Text = "Loaded";
+            }
+            catch (Exception ex)
+            {
+                lblState.Text = "Failed";
+                MessageBox.Show(this, ex.ToString(), "DICOM load error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally { UseWaitCursor = false; }
         }
     }
 }
